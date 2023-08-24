f1a4ff3afb9875ce931cc65f699fee6e09ca7efd
Kevin_alvarez1 <3871081810101@ingenieria.usac.edu.gt>
2023-08-24 14:58:03 -0600

    Primera carga, menu admin profesores casi completo

---
+++ IPC1_2S2023_Proyecto1_202203038.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ipc1_2s2023_proyecto1_202203038;
+
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+public class IPC1_2S2023_Proyecto1_202203038 {
+
+
+    public static void main(String[] args) {
+  
+        MenuPrincipal Aparece = new MenuPrincipal();
+        Aparece.setVisible(true);
+           
+    
+    }
+}
+
+    
+
+    
+
